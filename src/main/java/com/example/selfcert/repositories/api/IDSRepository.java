7647a7b3d86ec36886190ae8c0f981a3733f0147
Chandima Jayawickrema <cjayawickrema@gmail.com>
2022-06-10 18:25:08 +0530

    adding tests and custom gradle tasks

--- src/main/java/com/example/selfcert/repositories/api/IDSRepository.java
+++ src/main/java/com/example/selfcert/repositories/api/IDSRepository.java
@@ -6,21 +6,19 @@ import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class IDSRepository {
     private RestTemplate restTemplate = new RestTemplate();
     private final static int EPOCH_MULTIPLIER = 1000;
+    private final Random r = new Random();
 
     public Optional<Date> getLastLoggedInTime(String email) {
         ParameterizedTypeReference<HashMap<String, String>> responseType = new ParameterizedTypeReference<HashMap<String, String>>() {
         };
         RequestEntity<Void> request = RequestEntity.get("http://worldtimeapi.org/api/timezone/Asia/Colombo").accept(MediaType.APPLICATION_JSON).build();
         Map<String, String> jsonDictionary = restTemplate.exchange(request, responseType).getBody();
-        return Optional.of(new Date(Long.parseLong(jsonDictionary.get("unixtime")) * EPOCH_MULTIPLIER));
+        return Optional.of(new Date(Long.parseLong(jsonDictionary.get("unixtime")) * EPOCH_MULTIPLIER - (r.nextInt(1000 * 60 * 60 * 24 * 5))));
     }
 }
---
+++ src/test/java/com/example/selfcert/controllers/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.selfcert.controllers;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("functional")
+class UserControllerTest {
+
+    @Test
+    void getUserInformation() {
+    }
+}
